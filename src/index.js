42e34e600aaddda02bb5bd23295699c8be941ac4
gabrigode <gbarbosa7841@outlook.com>
2021-04-21 20:31:57 -0300

    Remove https server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const getImage = require("./getImage")
 const getBase64Image = require("./getBase64Image");
 
 const Twitter = new Twit({
-    consumer_key: process.env.consumer_key ,
+    consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
     access_token: process.env.access_token,
     access_token_secret: process.env.access_token_secret
@@ -12,7 +12,7 @@ const Twitter = new Twit({
 
 let imageInfo
 
-function getPost(){
+function getPost() {
     Promise.all([getImage])
         .then((data) => {
             imageInfo = data
@@ -20,19 +20,19 @@ function getPost(){
                 .then((imageData) => {
                     postToTwitter(imageData[0])
                 }
-            )
+                )
         }
-    ) 
+        )
 }
 
-function postToTwitter(imageData){
+function postToTwitter(imageData) {
     console.log("ImageInfo", imageInfo)
 
     Twitter.post('media/upload', { media_data: imageData }, function (err, data, response) {
 
         const mediaIdStr = data.media_id_string
         const meta_params = { media_id: mediaIdStr }
-   
+
         Twitter.post('media/metadata/create', meta_params, function () {
             try {
                 const params = { status: `Artist: ${imageInfo[0].tag_string_artist} \nSource: ${imageInfo[0].source}`, media_ids: [mediaIdStr] }
@@ -41,20 +41,13 @@ function postToTwitter(imageData){
                     console.log("New post", data)
                 })
             }
-            catch(error){
-                console.log ("A error has occurred while posting")
+            catch (error) {
+                console.log("A error has occurred while posting")
             }
         })
     })
 }
 
-setInterval(function(){ 
-	getPost()
+setInterval(function () {
+    getPost()
 }, 3600000);
-
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("DailyRem is running");
-});
-server.listen(3000);
